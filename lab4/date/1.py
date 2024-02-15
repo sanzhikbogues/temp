a345a9380345cc09eac0178970d89d43d2343605
sanzhikbogues <s_yerzhanov@kbtu.kz>
2024-02-15 17:03:01 +0600

    again unexpected changes

--- lab4/date/1.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime, timedelta
-today = datetime.today() - timedelta(days=5)
-print(today)
--- lab4/date/2.py
+++
@@ -1,4 +0,0 @@
-import datetime
-print(datetime.datetime.today() - datetime.timedelta(days=1))
-print(datetime.datetime.today())
-print(datetime.datetime.today() + datetime.timedelta(days=1))
