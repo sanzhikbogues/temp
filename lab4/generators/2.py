c7ae96dd658dee52a7c86b974fd522da982d16d6
sanzhikbogues <s_yerzhanov@kbtu.kz>
2024-02-21 21:40:17 +0600

    temp

--- lab4/generators/2.py
+++
@@ -1,9 +0,0 @@
-def even_numbers(n):
-    for i in range(n + 1):
-        if i % 2 == 0:
-            yield i
-
-n = int(input())
-even_gen = even_numbers(n)
-even_numbers_s = ', '.join(str(num) for num in even_gen)
-print("Even numbers between 0 and", n, "are:", even_numbers_s)
